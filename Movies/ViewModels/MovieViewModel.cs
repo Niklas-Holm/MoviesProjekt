ea6a1df17b09d49d7f87670ac16900d47b768d93
Niklas Holm <niklasholm04@gmail.com>
2025-08-14 15:23:43 +0200

    Added movieviewmodel and updated some of the MainWindow.axaml

---
+++ Movies/ViewModels/MovieViewModel.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using Movies.Models;
+
+namespace Movies.ViewModels;
+
+public class MovieViewModel
+{
+    public string Greeting { get; } = "Movies!";
+    public string Title { get; }
+    
+    private MovieGenre _genre;
+    public IEnumerable<MovieGenre> Genres { get; } = Enum.GetValues<MovieGenre>();
+    
+    public MovieGenre Genre
+    {
+        get => _genre;
+        set
+        {
+            if (_genre != value)
+            {
+                _genre = value;
+            }
+        }
+    }
+}
