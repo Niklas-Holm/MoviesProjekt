cd9f646a9a5eee356e1ef63d832d6049c3246017
Niklas Holm <niklasholm04@gmail.com>
2025-08-14 16:42:29 +0200

    Made a bunch of changes to make it work when running the application and added grid of movies

--- Movies/ViewModels/MovieViewModel.cs
+++ Movies/ViewModels/MovieViewModel.cs
@@ -1,28 +1,52 @@
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using Movies.Models;
 
 namespace Movies.ViewModels;
 
-public class MovieViewModel
+public class MovieViewModel : INotifyPropertyChanged
 {
-    public string Greeting { get; } = "Movies!";
-    public string Title { get; }
+    public string Greeting => "Movies!";
+    private string _title = string.Empty;
+    public string Title
+    {
+        get => _title;
+        set => SetField(ref _title, value);
+    }
+
+    private int _durationInMin;
+    public int DurationInMin
+    {
+        get => _durationInMin;
+        set => SetField(ref _durationInMin, value);
+    }
     
-    private MovieGenre _genre;
-    public IEnumerable<MovieGenre> Genres { get; } = Enum.GetValues<MovieGenre>();
-    
+    public IReadOnlyList<MovieGenre> Genres { get; } = Enum.GetValues<MovieGenre>();
+
+    // Valgt genre (init til noget ikke-null)
+    private MovieGenre _genre = MovieGenre.Action;
     public MovieGenre Genre
     {
         get => _genre;
-        set
-        {
-            if (_genre != value)
-            {
-                _genre = value;
-            }
-        }
+        set => SetField(ref _genre, value);
     }
     
-    public int DurationInMin { get; set; }
+    public ObservableCollection<Movie> Movies { get; }
+
+    public MovieViewModel()
+    {
+        Movies = new ObservableCollection<Movie>(Storage.GetMovies());
+    }
+    
+    public event PropertyChangedEventHandler? PropertyChanged;
+    protected bool SetField<T>(ref T field, T value, [CallerMemberName] string? name = null)
+    {
+        if (EqualityComparer<T>.Default.Equals(field, value)) return false;
+        field = value;
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        return true;
+    }
 }
